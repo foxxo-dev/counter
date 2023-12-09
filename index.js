58ec095840471beb1af75252935bcf09a463c09f
Foxxo Dev <gammert54@gmail.com>
2023-12-09 20:40:06 +0100

    Bug Fixes, Added Extra Number

--- index.js
+++ index.js
@@ -1,40 +1,39 @@
-const num1 = document.getElementById('_num1');
-let num1Target = 0;
 const num2 = document.getElementById('_num2');
 let num2Target = 0;
 const num3 = document.getElementById('_num3');
 let num3Target = 0;
-const num1_children = Array.from(num1.children);
+const num4 = document.getElementById('_num4');
+let num4Target = 0;
 
 setInterval(() => {
-  if (num3Target + 1 == 10) {
-    num3Target = 0;
-    if (num2Target + 1 == 10) {
-      num2Target = 0;
-      if (num1Target + 1 == 10) {
-        num1Target = 0;
+  if (num4Target + 1 === 10) {
+    num4Target = 0;
+    if (num3Target + 1 === 10) {
+      num3Target = 0;
+      if (num2Target + 1 === 10) {
+        num2Target = 0;
       } else {
-        num1Target++;
+        num2Target++;
       }
     } else {
-      num2Target++;
+      num3Target++;
     }
   } else {
-    num3Target++;
+    num4Target++;
   }
-  Array.from(num3.children).forEach((child, index) => {
-    child.style.transform = `translateY(-${num3Target}2.25rem`;
+  Array.from(num4.children).forEach((child) => {
+    child.style.transform = `translateY(-${num4Target}2.25rem)`;
+  });
+}, 1000);
+
+setInterval(() => {
+  Array.from(num3.children).forEach((child) => {
+    child.style.transform = `translateY(-${num3Target}2.25rem)`;
   });
 }, 1000);
 
 setInterval(() => {
   Array.from(num2.children).forEach((child, index) => {
     child.style.transform = `translateY(-${num2Target}2.25rem`;
-  });
-}, 1000);
-
-setInterval(() => {
-  Array.from(num1.children).forEach((child, index) => {
-    child.style.transform = `translateY(-${num1Target}2.25rem`;
   });
 }, 1000);
